efb889530bca5190001762c6d1760b4cec252ad7
Vijay Machiraju <vijayma@microsoft.com>
2019-03-25 12:52:05 -0400

    Initial version of Docker project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pipelines_dotnet_core_docker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
