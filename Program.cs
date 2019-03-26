dbf36e49745f9df7255ad75e63f04f946aeec054
Vijay Machiraju <vijay.machiraju@microsoft.com>
2019-03-26 13:30:38 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
-namespace pipelines-dotnet-core-docker
+namespace pipelines_dotnet_core_docker
 {
     public class Program
     {
